d53481d5cbd1459510a9c282dc4cb24f447fd307
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-08-04 21:17:09 -0700

    fix background for media queries to mobile

--- src/App.js
+++ src/App.js
@@ -2,37 +2,53 @@ import React from 'react';
 
 import CardGrid from './Components/cardGrid';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 import './App.css';
-import bgrnd from "./sw-bg.jpg";
+import bgrnd from './sw-bg.jpg';
+import xwing from './x-wing.jpg';
 
+const Background = styled.div`
+	background-image: url(${bgrnd});
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-attachment: fixed;
+	flex: 1 0 auto;
 
-const Container = styled.section`
-  background-image: url(${bgrnd});
-  background-size: cover;
-  display: flex;
-  flex-direction: column;
-  flex-wrap:wrap;
-  width: 100%;
-  height:100vh;
+	@media (max-width: 600px) {
+		background-image: url(${xwing});
+		background-size: cover;
+		background-position: center;
+		background-attachment: fixed;
+    flex: 1 0 auto;
+    padding: 20px 0;
+	}
 `;
 
+const Container = styled.div`
+  height: 100vh;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	box-sizing: border-box;
+	
+`;
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
 
-  return (
-    <div className="App">
-      <Container>
-        <h1 className="Header">React Wars</h1>
-        <CardGrid />
-      </Container>
-    </div>
-  );
-}
+	return (
+		<div className='App'>
+			<Container>
+				<Background>
+					<CardGrid />
+				</Background>
+			</Container>
+		</div>
+	);
+};
 
 export default App;
