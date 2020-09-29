fb04cef66abe83b5d8f74eb9bfe04b36a3267f7f
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-09-29 13:21:56 -0700

    add eslint to project. Maneuver types

--- src/Components/CharacterCard.tsx
+++ src/Components/CharacterCard.tsx
@@ -1,35 +1,44 @@
 import * as React from "react";
 import { Card } from "./Styled";
 
-type InfoNode = {
-	
-	id: number
+export type InfoNode = {
+	key: number;
 	person: string;
-	birth_year: string;
+	birthYear: string;
 	height: string;
 	mass: string;
-	skin_color: string;
-}
+	skinColor: string;
+};
 
-const CharacterCard: React.FC<InfoNode> = ({person, birth_year,height,mass, skin_color}) => {
+const CharacterCard: React.FC<InfoNode> = ({
+	person,
+	birthYear,
+	height,
+	mass,
+	skinColor,
+}: InfoNode) => {
 	return (
 		<Card>
 			<h2>{person}</h2>
-    <h3>
-    Birth Year:{birth_year}
-  </h3>
+			<h3>
+				Birth Year:
+				{birthYear}
+			</h3>
 			<h4>
-    Height:{height}
-    cm
-</h4>
+				Height:
+				{height}
+				cm
+			</h4>
 			<h4>
-    Mass:{mass}
-    kg
-</h4>
-    <h4>
-    Skin tone:{skin_color}
-  </h4>
-  </Card>
+				Mass:
+				{mass}
+				kg
+			</h4>
+			<h4>
+				Skin tone:
+				{skinColor}
+			</h4>
+		</Card>
 	);
 };
 
---
+++ src/Components/interface.tsx
@@ -0,0 +1,10 @@
+interface InfoNode {
+	key: number;
+	id: number;
+	person: string;
+	birthYear: string;
+	height: string;
+	mass: string;
+	skinColor: string;
+}
+export default "InfoNode";
